135b569d0de6672ef8496b22b1c198de4091e4d3
Fendo181 <fendo181git@gmail.com>
2019-04-22 09:36:50 +0900

    一旦初期状態に戻す

--- mysql/create_table_users.sql
+++ mysql/create_table_users.sql
@@ -8,15 +8,15 @@ create table users (
 );
 
 -- nameカラムの後ろに新しくemailカラムを追加する
-alter table users add column email varchar(255) after name;
+-- alter table users add column email varchar(255) after name;
 -- scoreカラムを削除する
-alter table users drop column score ;
+-- alter table users drop column score ;
 -- nameの名前や属性を変更する
-alter table users  name user_name varchar(80) default 'nobody';
+-- alter table users  name user_name varchar(80) default 'nobody';
 
 desc users;
 
 -- tableの名前を変更する
-alter table users rename persons;
-show tables;
-drop table if exists persons;
+-- alter table users rename persons;
+-- show tables;
+-- drop table if exists persons;
