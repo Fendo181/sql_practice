029676d5f016af264fad97e25603d0ce5b1cbd0f
Fendo181 <fendo181git@gmail.com>
2019-06-24 09:24:48 +0900

    テンプレートになるsqlファイルを作成した

---
+++ mysql/template_create_table.sql
@@ -0,0 +1,17 @@
+drop table if exists users;
+drop table if exists users_with_team;
+
+create table users (
+    id int unsigned primary key auto_increment,
+    name varchar(20),
+    score float
+);
+
+insert into users(name,score)values
+    ('endu',5.8),
+    ('koji',null),
+    ('kikuchi',4.9),
+    ('hoge',9.2),
+    ('hoge',.28),
+    ('sakita',3.0)
+;
